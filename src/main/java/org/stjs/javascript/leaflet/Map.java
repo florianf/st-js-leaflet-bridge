7236768f58f419a9cdd1fb063b9ac56c37b53b77
florianf <florian.falkner@general-solutions.at>
2013-12-10 16:55:03 +0100

    added more classes for events, options.

--- src/main/java/org/stjs/javascript/leaflet/Map.java
+++ src/main/java/org/stjs/javascript/leaflet/Map.java
@@ -3,6 +3,7 @@ package org.stjs.javascript.leaflet;
 import org.stjs.javascript.annotation.Namespace;
 import org.stjs.javascript.annotation.STJSBridge;
 import org.stjs.javascript.dom.Element;
+import org.stjs.javascript.functions.Callback1;
 
 @STJSBridge()
 @Namespace("L")
@@ -17,4 +18,17 @@ public class Map {
 	
 	public native void setView(LatLng center);
 	public native void setView(LatLng center, int zoom);
+	public native void setView(LatLng center, ZoomPanOptions options);
+	
+	public native void zoomIn(int delta);
+	public native void zoomIn(int delta, ZoomOptions options);
+
+	public native void zoomOut(int delta);
+	public native void zoomOut(int delta, ZoomOptions options);
+	
+	public native void setZoomAround(LatLng latLng, int zoom);
+	public native void setZoomAround(LatLng latLng, int zoom, ZoomOptions options);
+	
+	public native void addEventListener(String type, Callback1 fn);
+	
 }
